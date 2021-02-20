04ae43c701775a3ff36fd6584707094d10be796f
AlexTutberidze <sandrotutberidze18@gmail.com>
2021-02-20 19:19:10 +0400

    Build script changed

--- build/build.py
+++ build/build.py
@@ -8,17 +8,15 @@ if platform == "linux" or platform == "linux2":
     os.system("sudo apt-get update")
     os.system("sudo apt-get install libsfml-dev")
     os.system("[ -f /usr/bin/g++ ] && echo g++ is already installed || sudo apt install -y g++")
+    os.system("g++ ../src/tbg_main_player.cpp ../src/tbg_network.cpp -o ../bin/run -I ../include -lsfml-graphics -lsfml-window -lsfml-system")
 
 elif platform == "darwin":
 
     os.system("brew install sfml")
     os.system("[ -f /usr/bin/g++ ] && echo g++ is already installed || brew install g++")
+    os.system("g++ -std=c++11 ../src/tbg_main_player.cpp  -o ../bin/run -I ../include -lsfml-graphics -lsfml-window -lsfml-system")
 
-
-
-os.system("g++ ../src/tbg_main_player.cpp ../src/tbg_network.cpp -o ../bin/run -I ../include -lsfml-graphics -lsfml-window -lsfml-system")
-
-os.system('clear') 
+os.system('clear')
 
 # os.system("./../bin/server.exe")
 # os.system("./../bin/client.exe")
