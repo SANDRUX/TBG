63b177986a21739e73668a50eac6745dcb517b54
AlexTutberidze <sandrotutberidze18@gmail.com>
2021-02-22 13:45:19 +0400

    Test button Plugin has been added and bash script is under Controll

--- src/test/button.cpp
+++ src/test/button.cpp
@@ -1,8 +1,50 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode::getDesktopMode(),"Test Button", sf::Style::Default);
     
+    sf::RectangleShape shape(sf::Vector2f(50.f, 50.f));
+
+    sf::Vector2f pos(0.f, 0.f);
+
+    shape.setFillColor (sf::Color::Red);
+    shape.setPosition(pos);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while(window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {   
+                std::cout << "Button is pressed!" << std::endl;
+                // sf::Vector2i micePos = sf::Mouse::getPosition();
+
+                // if (micePos.x >= float(0) && micePos.x < float(50) && micePos.y >= float(0) && micePos.y < float(50))
+                // {
+                //     std::cout << "Button is pressed!" << std::endl;
+                //     std::cout << micePos.x << " " << micePos.y << std::endl;
+                // }                
+            } 
+        }
+
+        window.clear(sf::Color::Cyan);
+
+        window.draw(shape);
+
+        // std::cout << "Something" << std::endl;
+
+        window.display();
+    }
+    
+
     return 0;
 }
---
+++ src/test/test_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ button.cpp -o button.exe -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -pthread
