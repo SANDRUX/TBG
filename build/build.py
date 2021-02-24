67f75db153d877d058471fb619baa197a9fe8e64
AlexTutberidze <sandrotutberidze18@gmail.com>
2021-02-24 10:11:22 +0400

    Build script separated

--- build/build.py
+++ build/build.py
@@ -5,17 +5,11 @@ from sys import platform
 
 if platform == "linux" or platform == "linux2":
 
-    os.system("sudo apt-get update")
-    os.system("sudo apt-get install libsfml-dev")
-    os.system("[ -f /usr/bin/g++ ]")
-    os.system("[ -f /usr/bin/g++ ] && echo g++ is already installed || sudo apt install -y g++")
-    os.system("g++ ../src/Player/Player.cpp ../src/Network/Network.cpp ../src/System/System.cpp -o ../bin/run -I ../include -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -pthread")
+    os.system("./linux_build.sh")    
 
 elif platform == "darwin":
 
-    os.system("brew install sfml")
-    os.system("[ -f /usr/bin/g++ ] && echo g++ is already installed || brew install g++")
-    os.system("g++ ../src/Player/Player.cpp ../src/Network/Network.cpp ../src/System/System.cpp -o ../bin/run -I ../include -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -pthread")
+    os.system("./x_build.sh")
 
 os.system('clear')
 
---
+++ build/x_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export TBG_INCLUDE=$HOME/TBG/iclude
+export TBG_SRC=$HOME/TBG/SRC
+
+brew install sfml
+
+[ -f /usr/bin/g++ ] && echo g++ is already installed || brew install g++
+g++ $TBG_SRC/Player/Player.cpp $TBG_SRC/Network/Network.cpp TBG_SRC/System/System.cpp -o ../bin/run -I $TBG_INCLUDE -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -pthread
