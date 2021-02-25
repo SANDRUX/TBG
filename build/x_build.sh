142e7dff1f7ee68674605f3dcefb67c4cc16342d
AlexTutberidze <sandrotutberidze18@gmail.com>
2021-02-25 10:15:11 +0400

    The Build Script has rewritten on Bash

--- build/x_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export TBG_INCLUDE=$HOME/TBG/include
-export TBG_SRC=$HOME/TBG/src
-
-brew install sfml
-
-[ -f /usr/bin/g++ ] && echo g++ is already installed || brew install g++
-g++ $TBG_SRC/Player/Player.cpp $TBG_SRC/Network/Network.cpp $TBG_SRC/System/System.cpp -o ../bin/run -I $TBG_INCLUDE/. -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -pthread
