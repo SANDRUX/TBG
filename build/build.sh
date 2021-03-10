d4afe5ce6897108c8b88f3fff75ee4df60373fb5
AlexTutberidze <sandrotutberidze18@gmail.com>
2021-03-10 13:11:54 +0400

    Fixed compiling issue in build.sh script file

--- build/build.sh
+++ build/build.sh
@@ -16,4 +16,4 @@ export TBG_DEPEND=$HOME/TBG/libs/SFML-2.5.1
         [ -f /usr/bin/g++ ] && echo g++ is already installed || brew install g++
     fi
 
-    g++ $TBG_SRC/player/player.cpp $TBG_SRC/player/bad_guy.cpp $TBG_SRC/network/network.cpp $TBG_SRC/system/loop.cpp $TBG_SRC/system/thread.cpp -o ../bin/run -I $TBG_INCLUDE/. -I $TBG_DEPEND/include/. -L $TBG_DEPEND/lib/. -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -pthread
+    g++ $TBG_SRC/player/player.cpp $TBG_SRC/player/bad_guy.cpp $TBG_SRC/network/network.cpp $TBG_SRC/system/loop.cpp $TBG_SRC/system/thread.cpp -o $HOME/TBG/bin/run -I $TBG_INCLUDE/. -I $TBG_DEPEND/include/. -L $TBG_DEPEND/lib/. -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -pthread
