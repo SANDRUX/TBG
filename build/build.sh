0f38650a345cede3111cc073a959f8385039bb1f
AlexTutberidze <sandrotutberidze18@gmail.com>
2021-03-12 21:25:42 +0400

    Package configuration changed

--- build/build.sh
+++ build/build.sh
@@ -8,8 +8,6 @@ export TBG_DEPEND=$HOME/TBG/libs/SFML-2.5.1
 
     if [[ ${unameOut} -eq Linux ]]
     then
-        which apt || (echo "apt not installed" && exit)
-
         [ -f /usr/bin/g++ ] && echo g++ is already installed || (echo missing GNU GCC compiler check the INSTALL.md file)
         g++ $TBG_SRC/player/player.cpp $TBG_SRC/player/bad_guy.cpp $TBG_SRC/network/network.cpp $TBG_SRC/system/loop.cpp $TBG_SRC/system/thread.cpp -o $HOME/TBG/bin/run -I $TBG_INCLUDE/. -I $TBG_DEPEND/include/. -L $TBG_DEPEND/lib/. -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -pthread
 
